67ff67f08c369c0829d5441d2110ae74da5f1971
Nicholas Cook <njcook8@gmail.com>
2017-10-10 21:03:09 -0400

    Initial commit

---
+++ app/src/main/java/com/souvenotes/souvenotes/LoginActivity.kt
@@ -0,0 +1,15 @@
+package com.souvenotes.souvenotes
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * Created by NicholasCook on 10/10/17.
+ */
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
